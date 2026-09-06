6a3c4d9d3967cf369da97bd3ec61f03e2a0b77ce
sodissacco-lab <sodis.sacco@gmail.com>
2026-09-06 13:52:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
 // Project Settings → API → Project URL / anon public key
 // ============================================================
 const SUPABASE_URL = "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_PUBLIC_KEY_HERE";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jbWx6Y2F2b2JpdWpibWlkcGtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2ODAyODUsImV4cCI6MjEwNDI1NjI4NX0.wfo7BDHagVQMAkN0xMakFCy5KtJpmpZEx-ItiszkK-U";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
