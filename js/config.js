032e946ee002a0b1c03ba87aabeb83e85c40d4ed
sodissacco-lab <sodis.sacco@gmail.com>
2026-09-06 15:48:04 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,3 +8,4 @@ const SUPABASE_URL = "https://ocmlzcavobiujbmidpks.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jbWx6Y2F2b2JpdWpibWlkcGtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2ODAyODUsImV4cCI6MjEwNDI1NjI4NX0.wfo7BDHagVQMAkN0xMakFCy5KtJpmpZEx-ItiszkK-U";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabaseClient = supabaseClient;
